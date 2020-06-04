babc38ffed30675085203342d1f75c8513fe182b
Youjie <theforbiddenwaltz.thecomposer@gmail.com>
2020-06-05 05:55:26 +0800

    Add overload for INIFile.GetValue

--- src/Primrose/Primitives/FileFormat/INI/Helpers/Getters/GetValue.cs
+++ src/Primrose/Primitives/FileFormat/INI/Helpers/Getters/GetValue.cs
@@ -11,7 +11,7 @@ namespace Primitives.FileFormat.INI
     /// <param name="defaultValue">The default value</param>
     /// <returns>The value belonging to the section and key in the INIFile. If the key does not exist, returns defaultValue</returns>
     //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public T GetValue<T>(string section, string key, T defaultValue)
+    public T GetValue<T>(string section, string key, T defaultValue = default(T))
     {
       return Parser.Parse(GetString(section, key), defaultValue);
     }
