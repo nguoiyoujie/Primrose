81880599e7ce137938079b0731037da119a26926
Youjie <theforbiddenwaltz.thecomposer@gmail.com>
2020-06-10 06:40:59 +0800

    Add description

--- src/Primrose/Primitives/Triggers/Events.cs
+++ src/Primrose/Primitives/Triggers/Events.cs
@@ -1,11 +1,19 @@
 ﻿namespace Primrose.Primitives.Triggers
 {
+  /// <summary>An event delegate for handling condition updates</summary>
+  /// <param name="args"></param>
   public delegate void ConditionUpdateEventHandler(ConditionUpdateEventArgs args);
 
+  /// <summary>Represents a condition update</summary>
   public struct ConditionUpdateEventArgs
   {
+    /// <summary>The condition being updated</summary>
     public readonly ICondition Condition;
+
+    /// <summary>Indicates whether the condition is currently met</summary>
     public readonly bool Fulfilled;
+
+    /// <summary>Represents a condition update</summary>
     public ConditionUpdateEventArgs(ICondition condition, bool fulfilled) { Condition = condition; Fulfilled = fulfilled; }
   }
 }
