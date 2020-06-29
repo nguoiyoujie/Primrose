6478895ae878974fe60b166016b6b03c63740b7a
Youjie <theforbiddenwaltz.thecomposer@gmail.com>
2020-06-29 16:39:30 +0800

    Build

--- src/Primrose/Build.cs
+++ src/Primrose/Build.cs
@@ -5,7 +5,7 @@ namespace Primrose
   public static class Build 
   { 
    /// <summary>The time of build</summary> 
-   public const string Time = "Mon 06/29/2020 15:57:38.82"; 
+   public const string Time = "Mon 06/29/2020 16:39:06.21"; 
    /// <summary>The date of build in YYYYMMDD format</summary> 
    public const string BuildDate = "20200629"; 
    /// <summary>The daily revision number of the build</summary> 
