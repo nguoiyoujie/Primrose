32fea806497d26fe57a77ff7742863c459f59b5d
nguoiyoujie <yj.nguoi@yahoo.com>
2023-11-22 20:38:15 +0800

    Implement generic array struct enumerator with variable start and end indices

--- src/Primrose/Primitives/StaticEnumerators.cs
+++ src/Primrose/Primitives/StaticEnumerators.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Primrose.Primitives
 {
@@ -31,6 +34,66 @@ namespace Primrose.Primitives
 
     /// <summary>Retrieves the current item</summary>
     public T Current { get { return (current == null) ? default : current.Value; } }
+  }
+
+  /// <summary>Represents an enumerator of items in an array</summary>
+  /// <typeparam name="T"></typeparam>
+  public struct ArrayEnumerator<T> : IEnumerator<T>
+  {
+    private T[] _array;
+    private int _startindex;
+    private int _index;
+    private int _maxindex;
+
+
+    /// <summary>Creates an array enumerator</summary>
+    /// <param name="array">The backing</param>
+    /// <param name="index">The starting index</param>
+    /// <param name="maxindex">The maximum index</param>
+    public ArrayEnumerator(T[] array, int index = 0, int maxindex = -1)
+    {
+      _array = array;
+      _startindex = index - 1;
+      _index = _startindex;
+      _maxindex = (maxindex == -1) ? _array.Length - 1 : maxindex;
+      if (_array == null)
+      {
+        throw new ArgumentNullException(nameof(array));
+      }
+
+      if (_startindex < 0 || _maxindex < _startindex || _maxindex >= _array.Length)
+      {
+        throw new ArgumentException("Attempted to create an ArrayEnumerator<T> with indices outside the array capacity");
+      }
+    }
+
+    /// <summary>Disposes the enumerator</summary>
+    public void Dispose()
+    {
+      _index = _maxindex;
+    }
+
+    /// <summary>Advances the enumerator to the next element of the collection</summary>
+    /// <returns>true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection</returns>
+    public bool MoveNext()
+    {
+      _index++;
+      return _index <= _maxindex;
+    }
+
+    /// <summary>Sets the enumerator to its initial position, which is before the first element in the collection</summary>
+    public void Reset()
+    {
+      _index = _startindex;
+    }
+
+    /// <summary>Gets the current element in the collection</summary>
+    public T Current
+    {
+      get { return _array[_index]; }
+    }
+
+    object IEnumerator.Current => Current;
   }
 
 }
